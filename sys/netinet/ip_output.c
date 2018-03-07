15f5083717e2e9b27576d98360ce83b855b6fbf0
tj <tj@enoti.me>
2018-03-07 13:14:25 +0000

    Calculate udp pseudo header using udplen iplen is not always the same as udplen, use the udp len when calculating the udp checksum

--- sys/netinet/ip_output.c
+++ sys/netinet/ip_output.c
@@ -80,6 +80,10 @@ __FBSDID("$FreeBSD$");
 #include <netinet/in_var.h>
 #include <netinet/ip_var.h>
 #include <netinet/ip_options.h>
+
+#include <netinet/udp.h>
+#include <netinet/udp_var.h>
+
 #ifdef SCTP
 #include <netinet/sctp.h>
 #include <netinet/sctp_crc32.h>
@@ -928,14 +932,22 @@ void
 in_delayed_cksum(struct mbuf *m)
 {
 	struct ip *ip;
-	uint16_t csum, offset, ip_len;
+	struct udpiphdr *up;
+	uint16_t csum, offset, cklen = 0;
 
 	ip = mtod(m, struct ip *);
 	offset = ip->ip_hl << 2 ;
-	ip_len = ntohs(ip->ip_len);
-	csum = in_cksum_skip(m, ip_len, offset);
-	if (m->m_pkthdr.csum_flags & CSUM_UDP && csum == 0)
-		csum = 0xffff;
+
+	if (m->m_pkthdr.csum_flags & CSUM_UDP) {
+		up = mtod(m, struct udpiphdr *);
+		cklen = ntohs(up->ui_u.uh_ulen) + ip->ip_hl;
+		csum = in_cksum_skip(m, cklen, offset);
+		if (csum == 0)
+			csum = 0xffff;
+	} else {
+		cklen = ntohs(ip->ip_len);
+		csum = in_cksum_skip(m, cklen, offset);
+	}
 	offset += m->m_pkthdr.csum_data;	/* checksum offset */
 
 	/* find the mbuf in the chain where the checksum starts*/
